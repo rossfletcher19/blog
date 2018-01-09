557d7c1662454c78b09a48dc59174814f0a42e38
Aaron Ross and Ross Fletcher <ross@>
2018-01-09 09:37:02 -0800

    add new organizational layout and a few new get routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,15 +2,36 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import com.sun.scenario.effect.Blend;
 import models.Post;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import javax.print.DocFlavor;
+
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
+        //get: show new post form
+        get("/posts/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "newpost-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //post: process new post form
+        post("/posts/new", (request, response) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<String, Object>();
+            String content = request.queryParams("content");
+            Post newPost = new Post(content);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        //get: show all posts
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             ArrayList<Post> posts = Post.getAll();
@@ -18,11 +39,21 @@ public class App {
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-        post("/posts/new", (request, response) -> { //URL to make new post on POST route
-            Map<String, Object> model = new HashMap<String, Object>();
-            String content = request.queryParams("content");
-            Post newPost = new Post(content);
-            return new ModelAndView(model, "success.hbs");
+        //get: show an individual post
+        get("/posts/:id", (request, response) ->{
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToFind = Integer.parseInt(request.params("id"));
+            Post foundPost = Post.findById(idOfPostToFind);
+            model.put("post", foundPost);
+            return new ModelAndView(model, "post-detail.hbs");
         }, new HandlebarsTemplateEngine());
+
+        //get: show a form to update a post
+
+        //post: process a form to update a post
+
+        //get: delete and individual post
+
+        //get: delete all posts
     }
 }
