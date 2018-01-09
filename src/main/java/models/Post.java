471002735074cfde80e592598a11d39a2ec2fb8d
Aaron Ross and Ross Fletcher <ross@>
2018-01-09 08:37:08 -0800

    add more backend functionality

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 /**
@@ -7,23 +8,47 @@ import java.util.ArrayList;
  */
 public class Post {
 
-    private String content;
+    private final String content;
     private static ArrayList<Post> instances = new ArrayList<>();
+    private boolean published;
+    private LocalDateTime createdAt;
+    private int id;
 
-    public Post (String content){
+    public Post(String content) {
         this.content = content;
+        this.published = false;
+        this.createdAt = LocalDateTime.now();
         instances.add(this); //Also new. Can you figure out what I do and how I work?
+        this.id = instances.size();
+
     }
 
     public String getContent() {
         return content;
     }
 
-    public static ArrayList<Post> getAll(){
+    public static ArrayList<Post> getAll() {
         return instances;
     }
 
-    public static void clearAllPosts(){
+    public static void clearAllPosts() {
         instances.clear();
     }
+
+    public boolean getPublished() { //new too
+        return this.published;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Post findById(int id) {
+        return instances.get(id - 1); //why minus 1? See if you can figure it out.
+    }
+
 }
